f24eff4cf4f67311f04a77f730a7ec4a6b2b4f54
zhuakexi <zhuakexi@126.com>
2021-11-10 11:21:15 +0800

    adding support of custom color map

--- plot.py
+++ plot.py
@@ -18,7 +18,7 @@ def cdp_scatter(dat:pd.DataFrame)->go.Figure:
     fig.update_xaxes(type="log")
     return fig
 # cdps heatmap with cycle-phasing marker
-def cdps_plot(cdps,annote,sample_col="sample_name",order_col="index_order",group_col="group",hline=False):
+def cdps_plot(cdps,orig_annote,sample_col="sample_name",order_col="index_order",group_col="group",color_map=None,hline=False):
     """
     # plot cdps heatmap with group marker in different color
     # Input:
@@ -30,14 +30,30 @@ def cdps_plot(cdps,annote,sample_col="sample_name",order_col="index_order",group
     # Output:
     #    plotly Figure obj
     """
-    annote = annote.assign(
-        group_color = "blue"
-    )
-    annote.loc[annote[group_col]=="Post-M","group_color"] = "red"
-    annote.loc[annote[group_col]=="Pre-M","group_color"] = "goldenrod"
-    annote.loc[annote[group_col]=="early/mid-S","group_color"] = "green"
-    annote.loc[annote[group_col]=="mid-S/G2","group_color"] = "lightgreen"
-    annote = annote.sort_values(order_col)
+    if color_map == None:
+        # using default color map(for cell cycle)
+        annote = orig_annote.assign(
+            group_color = "blue"
+        )
+        annote.loc[annote[group_col]=="Post-M","group_color"] = "red"
+        annote.loc[annote[group_col]=="Pre-M","group_color"] = "goldenrod"
+        annote.loc[annote[group_col]=="early/mid-S","group_color"] = "green"
+        annote.loc[annote[group_col]=="mid-S/G2","group_color"] = "lightgreen"
+        annote = annote.sort_values(order_col)
+    elif isinstance(color_map,dict):
+        # using custom color map
+        annote = orig_annote.assign(
+            # grey for non assigned sample
+            group_color = "grey"
+        )
+        for key in color_map:
+            # assign group_color according to color_map
+            annote.loc[
+                annote[group_col] == key,
+                "group_color"
+            ] = color_map[key]
+    else:
+        raise ValueError("cdps_plot: Must provide color_map when set color_map to None.")
     order = list(annote[sample_col].values)
     fig = make_subplots(rows=2,cols=1,row_heights=[0.05,1],vertical_spacing=0.05)
     fig.add_trace(
