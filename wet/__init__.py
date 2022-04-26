f0ad725409eab3b16c6351a88f13d3269edcfc1d
zhuakexi <zhuakexi@126.com>
2022-04-26 16:33:02 +0800

    update entries

--- wet/__init__.py
+++ wet/__init__.py
@@ -1,4 +1,5 @@
 from .adtools import  gen_cache, gen_ad_from_cache
 from .afbb import task_stat, pick_useful
 from .meta_trick import merge_meta, last_mouse
-from .exp_record import parse_group_string, add_time_group_order
+from .exp_record import parse_group_string, ExpTime, add_group_order, add_group_hour, add_cell_type
+from .qc import basic_filter, plot_qc
