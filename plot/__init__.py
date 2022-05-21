e8871c9b45ed433ed991f269197c5d4b683aaf89
zhuakexi <zhuakexi@126.com>
2022-05-22 02:03:08 +0800

    fix typo

--- plot/__init__.py
+++ plot/__init__.py
@@ -1,3 +1,4 @@
 from .hic import plot_cool, plot_cools
 from .tech import tech_box
-from .plot import plot_cdps, plot_cdps_mark, time_attr, _plot_cluster_gapped_heatmap
+from .hic import plot_cdps
+from .plot import plot_cdps_mark, time_attr, _plot_cluster_gapped_heatmap
