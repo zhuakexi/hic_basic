332f6d3972c9b162c1afe226807d14ab9151aee0
zhuakexi <zhuakexi@126.com>
2022-06-08 16:15:36 +0800

    adding test to gen_adata

--- tests/test_gen_adata.py
+++ tests/test_gen_adata.py
@@ -1,6 +1,10 @@
 import os
 from numpy.testing import assert_allclose
-from wet.adtools import gen_adata
-def test_gen_adata(request):
-    expr = os.path.join(request.fspath.dirname, "data", "expr.csv.gz")
-    pass
+from .wet.adtools import gen_adata
+from .io import read_meta
+def test_gen_adata(request, tmp_path):
+    cache_dir = tmp_path / "test_gen_adata"
+    qc = read_meta(os.path.join(request.fspath.dirname, "data", "testqc.csv.gz"))
+    expr = os.path.join(request.fspath.dirname, "data", "counts.gene.tsv.gz")
+    adata = gen_adata(qc, cache_dir, expr = [expr])
+    assert adata.obs.shape[0] == 3
