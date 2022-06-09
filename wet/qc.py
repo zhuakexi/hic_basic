b47662d27fe2c2914ecca71e35beb801f3b35c47
zhuakexi <zhuakexi@126.com>
2022-06-09 20:28:46 +0800

    more flexible qc

--- wet/qc.py
+++ wet/qc.py
@@ -21,14 +21,32 @@ def basic_filter(meta):
     print("Keep %d samples from %d samples" % (qc.shape[0], meta.shape[0]))
     print("  with %d male: %d female." % ((qc["ypercent"] > 0.0005).sum(), (qc["ypercent"] <= 0.0005).sum()))
     return qc
-def plot_qc(meta):
+def c_u_filter(meta, ccol, ucol, c_threshold, u_threshold):
+    """
+    Filter out low-quality cells.
+    Input:
+        ccol: column name of contact number
+        ucol: column name of umis
+        c_threshold: [low, high] for contacts
+        u_threshold: [low, high] for contacts
+    Output:
+        new df with qc-passed rows
+    """
+    io_filter = (~meta[ccol].isna())&(~meta[ucol].isna())&(meta[ccol]>1)&(meta[ucol]>1)
+    metric_filter = (meta[ccol] > c_threshold[0]) & (meta[ccol] < c_threshold[1]) & (meta[ucol] < u_threshold[1]) & (meta[ucol] > u_threshold[0])
+    qc = meta.loc[io_filter & metric_filter]
+    print("Filter out %d samples with key-NA." % (~io_filter).sum())
+    print("Filter out %d samples with bad contacts or umis." % (~metric_filter).sum())
+    print("Keep %d samples from %d samples" % (qc.shape[0], meta.shape[0]))
+    return qc
+def plot_qc(meta, ccol="contacts", ucol="umis", c_threshold=[150_000, 1_000_000], u_threshold=[10_000, 600_000]):
     fig = px.scatter(meta,x="contacts",y="umis")
     fig.update_layout(
         height = 700,
         width = 700
     )
-    fig.add_hline(10_000)
-    fig.add_hline(600_000)
-    fig.add_vline(150_000)
-    fig.add_vline(1_000_000)
+    fig.add_hline(y=u_threshold[0], color="red")
+    fig.add_hline(y=u_threshold[1], color="red")
+    fig.add_vline(x=c_threshold[0], color="red")
+    fig.add_vline(x=c_threshold[1], color="red")
     return fig
