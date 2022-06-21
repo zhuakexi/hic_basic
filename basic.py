8628ae4d37cb240afb0128c17269a6e3216217c2
zhuakexi <zhuakexi@126.com>
2022-06-21 19:44:27 +0800

    purging and new basic module

--- basic.py
+++ basic.py
@@ -2,48 +2,23 @@ import pandas as pd
 import gzip
 import os
 from concurrent import futures
-def read_meta(fp):
+
+# All kinds of simple calculations on adata
+def add_contact_describe(adata, inplace=True):
     """
-    Read general metadata, take care of sample_name.
+    Get cell's contact decay statistics, defined in Nagano2017.
+    Input:
+        adata: AnnData object, must have uns["cdps"]
+    Output:
+        adata will extra obs cols
     """
-    df = pd.read_csv(fp, dtype={"sample_name":"string"},index_col=0)
-    df.index.name = "sample_name"
-    return df
-def divide_name(filename):
-    #home-made os.path.splitext, for it can't handle "name.a.b.c" properly
-    basename = os.path.basename(filename)
-    parts = basename.split(".") #split return >= 1 length list
-    if len(parts) == 1:
-        return parts[0], ""
+    bounds = adata.uns["cdps"].columns.astype(int)
+    short = adata.uns["cdps"].loc[:, (23_000 < bounds) & (bounds < 2_000_000)].sum(axis=1)
+    mitotic = adata.uns["cdps"].loc[:, (2_000_000 < bounds) & (bounds < 12_000_000)].sum(axis=1)
+    data = adata.obs.assign(short_r = short)
+    data = data.assign(mitotic_r = mitotic)
+    if inplace:
+        adata.obs = data
+        return None
     else:
-        return parts[0], "."+".".join(parts[1:]) 
-def parse_pairs(filename:str)->"Cell":
-    '''
-    read from 4DN's standard .pairs format
-    compatible with all hickit originated pairs-like format 
-    '''
-    #comment lines are stored in dataframe.attrs["comment"]
-    name_array = "readID chr1 pos1 chr2 pos2 strand1 strand2 phase0 phase1 phase_prob00 phase_prob01 phase_prob10 phase_prob11".split()
-    #read comment line
-    with gzip.open(filename,"rt") as f:
-        comments = []
-        for line in f.readlines():
-            if line[0] != "#":
-                break
-            comments.append(line)
-    #read table format data
-    pairs = pd.read_table(filename, header=None, comment="#",low_memory=False)
-    pairs.attrs["comments"] = comments
-    pairs.attrs["name"], _ = divide_name(filename) # get real sample name
-    #assign column names
-    pairs.columns = name_array[0:pairs.shape[1]]
-    #sys.stderr.write("pairs_parser: %s parsed \n" % filename)
-    return pairs
-def collect(meta:pd.DataFrame, name_col:str, file_col:str, func:"callable", threads)->pd.DataFrame:
-    # get cons for cells in dir
-    file_names = meta[file_col]
-    with futures.ProcessPoolExecutor(threads) as pool:
-        res = pool.map(func, file_names)
-    result = pd.concat(res, axis=1)
-    result.columns = meta[name_col]
-    return result
+        return data
