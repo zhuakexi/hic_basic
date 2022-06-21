c9e8938d703b575101e2ac6b75d1aa19a3a38ad5
zhuakexi <zhuakexi@126.com>
2022-06-21 19:44:06 +0800

    spli

--- plot/scanpy.py
+++ plot/scanpy.py
@@ -18,3 +18,27 @@ def sc_pca(adata, color):
         width = 500
     )
     return fig
+def plot_elbow(adata):
+    fig = px.scatter(adata.uns["pca"]["variance_ratio"])
+    fig.update_layout(
+        height = 500,
+        width = 500,
+        title = "PCA elbow plot"
+    )
+    return fig
+def plot_umap(adata, color):
+    """
+    Plot scanpy umap using plotly.
+    """
+    umap_res = pd.DataFrame(adata.obsm["X_umap"][:, :2])
+    umap_res.index = adata.obs_names
+    umap_res.columns = ["U1","U2"]
+    # adding annotations
+    umap_res = pd.concat([umap_res, adata.obs], axis=1)
+    # plot
+    fig = px.scatter(umap_res, x = "U1", y = "U2", color = color, hover_name = umap_res.index)
+    fig.update_layout(
+        height = 500,
+        width = 600
+    )
+    return fig
