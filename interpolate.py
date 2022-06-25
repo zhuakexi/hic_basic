9e018eb9629512c15180a719e6185662315f83ae
zhuakexi <zhuakexi@126.com>
2022-06-25 16:06:41 +0800

    adding and testing gaussian interpolate

---
+++ interpolate.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def _gaussian_interpolate(expr, traj, winSz, numPts):
+    """
+    Interpolation of the expression data along one trajectory.
+    Migrate from cellAlign package.
+    Input:
+        expr: raw single cell gene expression data (genes on rows, cells on columns)
+        traj: a vector of pseudo-time scores of the data-points whose length equals to the number of samples
+        winSz: window size of the interpolation
+        numPts: number of desired interpolated points
+    Output:
+        ValNewPts: interpolated expression data
+        trajValNewPts: trajectory values at the new points
+    TODO: adding interpolated error
+    """
+    def gaussian_weights(x, y):
+        return np.exp(-((x - y) ** 2) / winSz ** 2)
+    #remove NAs from the trajectory (if exist)
+    if np.isnan(traj).any():
+        traj = traj[~np.isnan(traj)]
+        expr = expr[~np.isnan(traj), :]
+    #generate interpolated values:
+    trajValNewPts = np.linspace(min(traj.values), max(traj.values), numPts)
+    weights = gaussian_weights(traj.values.reshape(290,1), trajValNewPts)
+    weights = weights/weights.sum(axis=0)
+    ValNewPts = np.dot(expr, weights)
+    return ValNewPts, trajValNewPts
+    #return ValNewPts, error, trajValNewPts
---
+++ tests/test_interpolate.py
@@ -0,0 +1,14 @@
+import os
+from numpy.testing import assert_allclose
+from ..interpolate import _gaussian_interpolate
+from ..io import read_meta
+def test_interpolate(request):
+    """
+    Test the interpolate function.
+    """
+    orig = read_meta(os.path.join(request.fspath.dirname, "data", "origVals.csv.gz"))
+    traj = read_meta(os.path.join(request.fspath.dirname, "data", "traj.csv.gz"))
+    traj = traj.iloc[:,0]
+    ref_res = read_meta(os.path.join(request.fspath.dirname, "data", "interpolatedVals.csv.gz"))
+    res, newtraj = _gaussian_interpolate(orig, traj, 0.1, 200)
+    assert_allclose(res, ref_res.values)
