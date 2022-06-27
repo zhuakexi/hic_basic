584b5ae34f16fbf04a2ed9d1d3a678b2e7f3fe02
zhuakexi <zhuakexi@126.com>
2022-06-27 17:00:11 +0800

    add_pmUMIs

--- basic.py
+++ basic.py
@@ -22,3 +22,17 @@ def add_contact_describe(adata, inplace=True):
         return None
     else:
         return data
+def add_pmUMIs(adata, inplace=True):
+    """
+    Add cell's g1 UMI ratio.
+    Input:
+        adata: AnnData object, must have ["g1_UMIs","g2_UMIs"]
+    Output:
+        adata will extra obs cols
+    """
+    data = adata.obs.assign(pmUMIs = adata.obs["g1_UMIs"]/(adata.obs["g1_UMIs"] + adata.obs["g2_UMIs"]))
+    if inplace:
+        adata.obs = data
+        return None
+    else:
+        return data
