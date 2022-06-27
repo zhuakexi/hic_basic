fix bug in mismatching index and wrong shape

interpolate.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def _gaussian_interpolate(expr, traj, winSz, numPts):
+def _gaussian_interpolate(expr, traj, winSz=0.1, numPts=200):
     """
     Interpolation of the expression data along one trajectory.
     Migrate from cellAlign package.
@@ -16,13 +16,16 @@ def _gaussian_interpolate(expr, traj, winSz, numPts):
     """
     def gaussian_weights(x, y):
         return np.exp(-((x - y) ** 2) / winSz ** 2)
-    #remove NAs from the trajectory (if exist)
+    # remove NAs from the trajectory (if exist)
+    # and make sure they have same sample index
     if np.isnan(traj).any():
         traj = traj[~np.isnan(traj)]
-        expr = expr[~np.isnan(traj), :]
+        expr = expr[:, ~np.isnan(traj)]
+    else:
+        expr = expr.loc[:, traj.index]
     #generate interpolated values:
     trajValNewPts = np.linspace(min(traj.values), max(traj.values), numPts)
-    weights = gaussian_weights(traj.values.reshape(290,1), trajValNewPts)
+    weights = gaussian_weights(traj.values.reshape(traj.shape[0], 1), trajValNewPts)
     weights = weights/weights.sum(axis=0)
     ValNewPts = np.dot(expr, weights)
     return ValNewPts, trajValNewPts