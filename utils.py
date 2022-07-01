30454f40f7f73bdfe44b6b4a39af6642d0a833f6
zhuakexi <zhuakexi@126.com>
2022-07-01 17:11:57 +0800

    moving gen_filies to global utils

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import six
 import re
+import os
 import numpy as np
 import pandas as pd
 
@@ -122,6 +123,33 @@ def binnify(chromsizes, binsize):
     )
 
     return bintable
+def gen_fileis(sample_table, dir_path, str_pat):
+    """
+    Check and generate fileis(input file tables storing file path) for downstream calculating.
+    Input:
+        sample_table: meta, sample_names as index, try to ensure all samples have their input file.
+        dir_path: where to find those input files.
+        str_pat: string pattern, gen exact file name. using {} to represent sample_name.
+    Output:
+        pd.DataFrame
+    """
+    if not sample_table.index.is_unique:
+        raise ValueError("[gen_fileis] Error: sample_table index is not unique")
+    a = dict(zip([str_pat.format(i) for i in sample_table.index], list(sample_table.index)))
+    b = set(os.listdir(dir_path))
+    hitting = a.keys() & b
+    missing_sample = [a[k] for k in (a.keys() - hitting)]
+    extra = list(b - hitting)
+    
+    if len(missing_sample):
+        print("[gen_fileis] Warning: can't finds files for %d samples:" % len(missing_sample))
+        print(",".join(missing_sample))
+    if len(extra):
+        print("[gen_fileis] Warning: find extra files for:")
+        print(",".join(extra[:20]))
+        if len(extra) > 20:
+            print("...")
+    return pd.Series([os.path.join(dir_path, k) for k in hitting], index=[a[k] for k in hitting])
 def _mouse_id_name_table(ref_file):
     """
     Get a table of mouse IDs and names from a reference file.
--- wet/utils.py
+++ wet/utils.py
@@ -30,30 +30,3 @@ def check_input(filesp, cols):
             print(",".join(missing[col]))
     if len(cols) == 1:
         return valid_samples[col], valid_files[col]
-def gen_fileis(sample_table, dir_path, str_pat):
-    """
-    Check and generate fileis(input file tables storing file path) for downstream calculating.
-    Input:
-        sample_table: meta, sample_names as index, try to ensure all samples have their input file.
-        dir_path: where to find those input files.
-        str_pat: string pattern, gen exact file name. using {} to represent sample_name.
-    Output:
-        pd.DataFrame
-    """
-    if not sample_table.index.is_unique:
-        raise ValueError("[gen_fileis] Error: sample_table index is not unique")
-    a = dict(zip([str_pat.format(i) for i in sample_table.index], list(sample_table.index)))
-    b = set(os.listdir(dir_path))
-    hitting = a.keys() & b
-    missing_sample = [a[k] for k in (a.keys() - hitting)]
-    extra = list(b - hitting)
-    
-    if len(missing_sample):
-        print("[gen_fileis] Warning: can't finds files for %d samples:" % len(missing_sample))
-        print(",".join(missing_sample))
-    if len(extra):
-        print("[gen_fileis] Warning: find extra files for:")
-        print(",".join(extra[:20]))
-        if len(extra) > 20:
-            print("...")
-    return pd.Series([os.path.join(dir_path, k) for k in hitting], index=[a[k] for k in hitting])
