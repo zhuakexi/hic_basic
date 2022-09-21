bfa8788617d5e8ff488218397deff096ae4a4b5a
zhuakexi <zhuakexi@126.com>
2022-09-21 21:08:56 +0800

    add selection_cent frame

--- threeD/sele_cent.py
+++ threeD/sele_cent.py
@@ -11,14 +11,32 @@ obj (string or PyMOL selection)
         to find the centromere particles
 genome (string)
         name of the reference genome
-
+flank (int)
 RETURNS:
 a newly created selection with name `obj`_cent.
 """
 
 from pymol import cmd
+from .utils import fetch_centromeres
 
 def sele_cent(obj, genome):
-    SelName = obj + "_cent"
-    pass
+    SelName = obj + "_centromeres"
+
+    # get all atoms (chrom, name)
+    # cmd.iterate(selection str, expression str, space=)
+    # expression string can use:
+    #   1. default exposed varaibles
+    #   2. stored.xxx
+    #   3. custom variable space (space = myspace)
+    myspace = {"bins":[]}
+    cmd.iterate('(all)', bins.append((chain, name)), space=myspace)
+    
+    # find atoms (chrom, name) in centromeres
+    
+    # select
+    cmd.select(SelName, "None")
+    # select right flanking according to chrom, name
+    # select left flanking according to chrom, name
+    
+    return SelName
 cmd.extend("sele_cent", sele_cent)
