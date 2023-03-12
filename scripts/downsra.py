8b905101020e569cf316f3eb28eb6b6a03b12da6
zhuakexi <zhuakexi@126.com>
2023-03-12 15:05:34 +0800

    adding multi backend because aspera just fail some time, using cached url and md5

--- scripts/downsra.py
+++ scripts/downsra.py
@@ -6,13 +6,19 @@ import json
 import re
 from time import sleep
 from subprocess import run
+from hashlib import md5
 import requests
 import pandas as pd
 info = sys.argv[1] # infomation csv file, must have ["Run", "name"] columns
 outdir = sys.argv[2] # output directory
+if len(sys.argv) > 3:
+    backend = sys.argv[3]
+else:
+    backend = "aspera"
 info = pd.read_csv(info)
 
 # --- get fastq url from accession number ---
+
 # generate request url
 def newname(url):
     match = re.search('/(SRR[\d]+)_?\d?.fastq.gz', url)
@@ -22,30 +28,83 @@ def newname(url):
     return name + ext
 
 json_urls=[
-    "https://www.ebi.ac.uk/ena/portal/api/filereport?accession="+accession+"&result=read_run&fields=fastq_aspera&format=json&download=true"
+    "https://www.ebi.ac.uk/ena/portal/api/filereport?accession="+accession+"&result=read_run&fields=fastq_aspera,fastq_ftp,fastq_md5&format=json&download=true"
     for accession in info["Run"]
 ]
+fastq_aspera_urls = []
 fastq_ftp_urls = []
+fastq_md5s = []
+print("Start searching urls...")
+# check cache
+if os.path.exists(os.path.join(outdir, "fastq_urls.json")):
+    print("Cache found, load %s" % os.path.join(outdir, "fastq_urls.json"))
+    cached = json.load(open(os.path.join(outdir, "fastq_urls.json")))
+else:
+    cached = {}
 for json_url, accession in zip(json_urls, info["Run"]):
-    fastq_ftp_urls.append(
-        json.loads(requests.get(url=json_url).text)[0]["fastq_aspera"].split(";")
+    if accession in cached:
+        print("Cache found for %s" % accession)
+        fastq_aspera_urls.append(cached[accession]["fastq_aspera_urls"])
+        fastq_ftp_urls.append(cached[accession]["fastq_ftp_urls"])
+        fastq_md5s.append(cached[accession]["fastq_md5s"])
+        continue
+    # search if not cached
+    dat = json.loads(requests.get(url=json_url).text)
+    fastq_aspera_urls.append(
+        dat[0]["fastq_aspera"].split(";")
     )
+    fastq_ftp_urls.append(
+        dat[0]["fastq_ftp"].split(";")
+    )
+    fastq_md5s.append(
+        dat[0]["fastq_md5"].split(";")
+    )
+    cached[accession] = {
+        "fastq_aspera_urls": fastq_aspera_urls[-1],
+        "fastq_ftp_urls": fastq_ftp_urls[-1],
+        "fastq_md5s": fastq_md5s[-1]
+    }
     sleep(0.3) # avoid too many requests
     print("Search url %s done." % accession)
+print("Save cache...")
+json.dump(cached, open(os.path.join(outdir, "fastq_urls.json"), "w"))
+
 # --- download fastq ---
+
 # generate readable file names
+fastq_aspera_urls_flatten = [j for i in fastq_aspera_urls for j in i]
 fastq_ftp_urls_flatten = [j for i in fastq_ftp_urls for j in i]
-outfiles = [os.path.join(outdir, newname(url)) for url in fastq_ftp_urls_flatten]
+fastq_md5s_flatten = [j for i in fastq_md5s for j in i]
+outfiles = [os.path.join(outdir, newname(url)) for url in fastq_aspera_urls_flatten]
 # generate download commands
-keyp = "~/miniconda3/envs/fastq-downloader/etc/asperaweb_id_dsa.openssh"
-commands = [
-    "ascp  -vQT -l 500m -P33001 -k 1 -i {keyp} era-fasp@{url} {outfile}".format(
-        keyp = keyp,
-        url = url,
-        outfile = outfile
-    )
-    for url, outfile in zip(fastq_ftp_urls_flatten, outfiles)
-]
+if backend == "aspera":
+    keyp = "~/miniconda3/envs/fastq-downloader/etc/asperaweb_id_dsa.openssh"
+    commands = [
+        "ascp  -vQT -l 500m -P33001 -k 1 -i {keyp} era-fasp@{url} {outfile}".format(
+            keyp = keyp,
+            url = url,
+            outfile = outfile
+        )
+        for url, outfile in zip(fastq_aspera_urls_flatten, outfiles)
+    ]
+elif backend == "wget":
+    commands = [
+        "wget -O {outfile} {url}".format(
+            url = url,
+            outfile = outfile
+        )
+        for url, outfile in zip(fastq_ftp_urls_flatten, outfiles)
+    ]
+elif backend == "curl":
+    commands = [
+        "curl -o {outfile} {url}".format(
+            url = url,
+            outfile = outfile
+        )
+        for url, outfile in zip(fastq_ftp_urls_flatten, outfiles)
+    ]
+else:
+    raise ValueError("Unknown backend.")
 # download
 for command, outfile in zip(commands, outfiles):
     if os.path.exists(outfile):
@@ -55,3 +114,35 @@ for command, outfile in zip(commands, outfiles):
     run(command, shell=True)
     sleep(0.3) # avoid too many requests
 print("All done.")
+# --- check md5 ---
+damaged = []
+for md5value, outfile in zip(fastq_md5s_flatten, outfiles):
+    if os.path.exists(outfile):
+        print("Checking %s" % outfile)
+        # if have newer md5 res cache, use it
+        if os.path.exists(outfile + ".md5") and os.path.getmtime(outfile + ".md5") > os.path.getmtime(outfile):
+            print("Use cached md5 result.")
+            with open(outfile + ".md5", "r") as f:
+                md5res = f.read().strip()
+            if md5res != md5value:
+                print("MD5 mismatch for %s" % outfile)
+                print("Expected: %s" % md5value)
+                print("Got: %s" % md5res)
+                damaged.append(outfile)
+            with open(outfile + ".md5", "w") as f:
+                f.write(md5res)
+            continue
+        with open(outfile, "rb") as f:
+            md5res = md5(f.read()).hexdigest()
+        if md5res != md5value:
+            print("MD5 mismatch for %s" % outfile)
+            print("Expected: %s" % md5value)
+            print("Got: %s" % md5res)
+            damaged.append(outfile)
+if len(damaged) > 0:
+    print("Damaged files found:")
+    print("\t" + " ".join(damaged))
+    exit(1)
+else:
+    print("All files are fine.")
+    exit(0)
