7af9344a81b13661d1db697c69d2bbd5c2672a3a
zhuakexi <zhuakexi@126.com>
2023-03-15 18:38:16 +0800

    release gen_adata from comment and delete basic_filter

--- wet/__init__.py
+++ wet/__init__.py
@@ -1,5 +1,5 @@
-#from .adtools import  gen_adata
+from .adtools import  gen_adata
 from .afbb import task_stat, pick_useful, check_RNA
 from .meta_trick import merge_meta, last_mouse
 from .exp_record import parse_group_string, ExpTime, add_group_order, add_group_hour, add_cell_type
-from .qc import basic_filter, plot_qc
+from .qc import plot_qc
