ab32f863298166e92ca3416bfc1772cc62408cd6
zhuakexi <zhuakexi@126.com>
2023-12-13 08:44:01 +0000

    adding cli saddle expected comaprtemnt

--- hic_basic/coolstuff.py
+++ hic_basic/coolstuff.py
@@ -382,6 +382,38 @@ def cli_balance(coolp, threads=8, force=False, conda_env=None, cwd=None):
     except subprocess.CalledProcessError as e:
         print(e.output)
         return False
+def cli_compartment(coolp, phasing_track, outprefix, view, conda_env=None, cwd=None):
+    if conda_env is None:
+        conda_run = ""
+    else:
+        conda_run = f"conda run -n {conda_env}"
+    if view is None:
+        view = ""
+    else:
+        view = f"--view {view}"
+    cmd = f"{conda_run} cooltools eigs-cis {view} --phasing-track {phasing_track} -o {outprefix} {coolp}"
+    subprocess.check_output(
+        cmd,
+        shell=True,
+        cwd = cwd
+    )
+    return f"{outprefix}.cis.vecs.tsv", f"{outprefix}.cis.lam.txt"
+def cli_saddle(coolp, eigv, expected, outprefix, view, conda_env=None, cwd=None):
+    if conda_env is None:
+        conda_run = ""
+    else:
+        conda_run = f"conda run -n {conda_env}"
+    if view is None:
+        view = ""
+    else:
+        view = f"--view {view}"
+    cmd = f"{conda_run} cooltools saddle --qrange 0.02 0.98 --fig png -o {outprefix} {view} --strength {coolp} {eigv} {expected}"
+    subprocess.check_output(
+        cmd,
+        shell=True,
+        cwd = cwd
+    )
+    return f"{outprefix}.saddledump.npz", f"{outprefix}.png"
 def cli_IS(coolp, output, windowsizes, conda_env=None, cwd=None):
     if conda_env is None:
         conda_run = ""
@@ -395,6 +427,7 @@ def cli_IS(coolp, output, windowsizes, conda_env=None, cwd=None):
         shell=True,
         cwd = cwd
     )
+    return output
 def cli_pileup(coolp, feature, output, format="BED", view=None, expected=None, flank=100000, conda_env=None, cwd=None, threads=8):
     if conda_env is None:
         conda_run = ""
@@ -429,22 +462,26 @@ def cli_pileup(coolp, feature, output, format="BED", view=None, expected=None, f
             cwd = cwd
         )
     return output
-def cli_expected(coolp, output, conda_env=None, cwd=None, threads=8):
+def cli_expected(coolp, output, view=None, conda_env=None, cwd=None, threads=8):
     if conda_env is None:
         conda_run = ""
     else:
         conda_run = f"conda run -n {conda_env}"
+    if view is None:
+        view = ""
+    else:
+        view = f"--view {view}"
     subprocess.run(
-        f"{conda_run} cooltools expected-cis -p {threads} -o {output} {coolp}",
+        f"{conda_run} cooltools expected-cis -p {threads} -o {output} {view} {coolp}",
         shell=True,
         cwd=cwd
     )
     return output
-def cli_ct_callTAD(filei,fileo):
-    """
-    cooltools(ct) call TAD
-    """
-    subprocess.check_output(
-        "conda run -n embryo cooltools diamond-insulation -o %s --append-raw-scores %s 100000" %(fileo,filei),
-        shell=True
-    )
+# def cli_ct_callTAD(filei,fileo):
+#     """
+#     cooltools(ct) call TAD
+#     """
+#     subprocess.check_output(
+#         "conda run -n embryo cooltools diamond-insulation -o %s --append-raw-scores %s 100000" %(fileo,filei),
+#         shell=True
+#     )
