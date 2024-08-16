38d42de60c6fe442c42774640471bf9f4e35986f
zhuakexi <zhuakexi@126.com>
2024-08-16 17:23:10 +0800

    add pipeline intermediate file helping functions

--- hic_basic/pipeline/rule.py
+++ hic_basic/pipeline/rule.py
@@ -1,6 +1,10 @@
 import itertools
 import json
+import math
 from pathlib import Path
+
+import numpy as np
+import pandas as pd
 # --- input --- #
 def safe_input(file_pat, with_key=False, **kwargs):
     """
@@ -83,6 +87,173 @@ def check_key(file_pat, key_to_check, **kwargs):
         else:
             print(f"Warning: {input_value} don't have all files")
     return good_values
+
+
+### --- intermediate files --- ###
+#TODO: add a function to generate the file pattern for the intermediate files from smk rules
+
+
+def check_exsit(path):
+    """
+    Check if a file exists.
+    Tolerate the pd.NA, None, math.nan, np.nan
+    """
+    if path is pd.NA:
+        res = False
+    elif path is None:
+        res = False
+    elif path is math.nan:
+        res = False
+    elif path is np.nan:
+        res = False
+    else:
+        try:
+            res = Path(path).exists()
+        except TypeError:
+            print(path)
+            res = False
+    return res
+def bubble_flow_touched():
+    """
+    This function returns a dictionary that maps the file paths pattern for
+    all the files that are touched by the bubble_flow pipeline.
+    """
+    DNA = [
+        "{task_dirp}/DNA/{sample}_R1.fq.gz",
+        "{task_dirp}/DNA/{sample}_R2.fq.gz"
+    ]
+    RNA = [
+        "{task_dirp}/RNA/{sample}_R1.fq.gz",
+        "{task_dirp}/RNA/{sample}_R2.fq.gz"
+
+    ]
+    hic_mapped = [
+        "{task_dirp}/hic_mapped/{sample}.sorted.bam",
+        "{task_dirp}/hic_mapped/{sample}.sorted.bam.bai"
+    ]
+    seg = [
+        "{task_dirp}/seg/{sample}.seg.gz"
+    ]
+    pre_seg = [
+        "{task_dirp}/seg/{sample}.seg.gz"
+    ]
+    pairs_0 = [
+        "{task_dirp}/pairs_0/{sample}.pairs.gz",
+        "{task_dirp}/pairs_0/{sample}.raw_pairs.gz"
+    ]
+    pairs_c1 = [
+        "{task_dirp}/pairs_c1/{sample}.c1.pairs.gz",
+    ]
+    pairs_c12 = [
+        "{task_dirp}/pairs_c12/{sample}.c12.pairs.gz",
+    ]
+    dip = [
+        "{task_dirp}/dip/{sample}.dip.pairs.gz",
+    ]
+    info = [
+        "{task_dirp}/info/{sample}.basic.info",
+        "{task_dirp}/info/{sample}.reads.info",
+        "{task_dirp}/info/{sample}.rna_reads.info",
+        "{task_dirp}/info/{sample}.dna_reads.info",
+        "{task_dirp}/info/{sample}.seg_stat.info",
+        ]
+    _3dg = []
+    for seed in [1,2,3,4,5]:
+        for reso in ["4m","1m","200k","50k","20k"]:
+            _3dg.append(
+                "{{task_dirp}}/3dg/{{sample}}.{reso}.{seed}.3dg".format(
+                    seed = seed,
+                    reso = reso
+                )
+            )
+    _3dg_c = []
+    for seed in [1,2,3,4,5]:
+        for reso in ["4m","1m","200k","50k","20k"]:
+            _3dg_c.append(
+                "{{task_dirp}}/3dg_c/{{sample}}.clean.{reso}.{seed}.3dg".format(
+                    seed = seed,
+                    reso = reso
+                )
+            )
+    mapper = {
+        "DNA" : DNA,
+        "RNA" : RNA,
+        "hic_mapped" : hic_mapped,
+        "seg" : seg,
+        "pre_seg" : pre_seg,
+        "info" : info,
+        "_3dg" : _3dg,
+        "_3dg_c" : _3dg_c,
+        "pairs_0" : pairs_0,
+        "pairs_c1" : pairs_c1,
+        "pairs_c12" : pairs_c12,
+        "dip" : dip
+    }
+    return mapper
+def symlink_files(sample, task_dirp, target_dir, omit=None, filepats=None):
+    """
+    Create symbolic links from a old task directory to a new task directory.
+    Use this to partially run the pipeline.
+    Input:
+        sample: the sample name
+        task_dirp: the old task directory
+        target_dir: the new task directory
+        omit: the pipeline steps to omit
+        filepats: key-value pairs of file patterns created by the pipeline,
+            if None, use the file patterns created by the bubble_flow
+            key: the pipeline step name
+            value: the file pattern, must contain {sample} and {task_dirp} as placeholders
+    Return:
+        None
+    """
+    if filepats is not None:
+        mapper = filepats
+    else:
+        mapper = bubble_flow_touched()
+    if omit is None:
+        subset = mapper.keys()
+    else:
+        subset = set(mapper.keys()) - set(omit)
+    #print(subset)
+    for key in subset:
+        for pat in mapper[key]:
+            from_ = pat.format(sample=sample, task_dirp=task_dirp)
+            to_ = pat.format(sample=sample, task_dirp=target_dir)
+            # only create the link when the original file exists
+            if check_exsit(from_):
+                # create the parent directory if not exists
+                Path(to_).parent.mkdir(parents=True, exist_ok=True)
+                if Path(to_).is_symlink():
+                    if Path(to_).resolve() == Path(from_).resolve():
+                        # do nothing when the link is already exist
+                        # and the link is the same
+                        pass
+                    else:
+                        # update the link when the link is already exist
+                        # but link to wrong file
+                        Path(to_).unlink()
+                        Path(to_).symlink_to(from_)
+                elif Path(to_).exists():
+                    # this happens when the new task is already run
+                    print(f"File {to_} already exists, skip")
+                    pass
+                else:
+                    Path(to_).symlink_to(from_)
+            else:
+                pass
+def unlink_files(sample, target_dir, subset=None):
+    mapper = bubble_flow_touched()
+    if subset is None:
+        subset = mapper.keys()
+    for key in subset:
+        for pat in mapper[key]:
+            to_ = Path(pat.format(sample=sample, task_dirp=target_dir))
+            if to_.is_symlink():
+                to_.unlink()
+            else:
+                pass
+
+
 # --- helpers --- #
 def json_reader(file):
     with open(file, "r") as f:
