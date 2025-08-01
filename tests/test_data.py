167d99c179701cfff6f6e70b383d674bdecbd8a2
zhuakexi <zhuakexi@126.com>
2025-08-01 13:53:31 +0800

    isolate feature code from data module

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 import pandas as pd
 from hic_basic.binnify import GenomeIdeograph
-from hic_basic.data import dupref_annote, fetch_cent_chromlen, TSS
+from hic_basic.data import dupref_annote, fetch_cent_chromlen
 
 class TestData(unittest.TestCase):
     """
@@ -44,51 +44,6 @@ class TestData(unittest.TestCase):
         cent_chromlen = fetch_cent_chromlen("mm10_dip")
         print(cent_chromlen)
         self.assertTrue("chr1(mat)" in cent_chromlen.index)
-class TestGenomeFeatures(unittest.TestCase):
-    """
-    Test the genome feature functions.
-    """
-    def setUp(self):
-        self.genome = "GRCh38"
-        self.gtf_path = "/share/Data/ychi/genome/GRCh38/gencode.v33.primary_assembly.annotation.gtf"
-        self.chrom_sizes_path = "/share/Data/ychi/genome/GRCh38/GRCh38.chr.len"
-        self.out_dir = Path(__file__).parent / "output" / "data"
-        if not self.out_dir.exists():
-            self.out_dir.mkdir(parents=True)
-    def test_get_tss_region_from_gtf(self):
-        """
-        Test the get_tss_region_from_gtf function.
-        """
-        tss_regions = TSS.get_tss_region_from_gtf(
-            self.gtf_path,
-            cache_file = self.out_dir / "tss_regions.csv",
-        )
-        print(tss_regions.head())
-    def test_TSS(self):
-        """
-        Test the TSS class.
-        """
-        tss = TSS(
-            self.genome
-        )
-        tss.compile(
-            self.gtf_path,
-            force = True
-        )
-        data = tss.data
-        print(data.head())
-    def test_TSS_cache(self):
-        """
-        Test the TSS class with cache.
-        """
-        tss = TSS(
-            self.genome
-        )
-        tss.compile(
-            self.gtf_path,
-            force = False
-        )
-        data = tss.data
-        print(data.head())
+
 if __name__ == "__main__":
     unittest.main()
