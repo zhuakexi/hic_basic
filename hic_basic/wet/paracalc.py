7d560965bd49dbc31c729d6d3dde2618633a118f
zhuakexi <zhuakexi@126.com>
2025-12-10 18:50:12 +0800

    add filter string to do like only-compute-autosomes

--- hic_basic/wet/paracalc.py
+++ hic_basic/wet/paracalc.py
@@ -42,14 +42,15 @@ def gen_cool(qc, cached, threads=16, sizef = "/share/home/ychi/data/genome/GRCm3
         json.dump(dict(zip(valid_samples, caching_files)), f)
     return dict(zip(valid_samples, caching_files))
 # --- generate contact decay profiles ---
-def gen_cdps(filesp, threads = 32, range_dtype='string', pairs_col="pairs_c123"):
+def gen_cdps(filesp, threads = 32, range_dtype='string', pairs_col="pairs_c123", filter_str=None):
     # --- check pairs file ---
     valid_samples, valid_files = check_input(filesp, pairs_col)
     # --- calculate --- 
     with futures.ProcessPoolExecutor(threads) as pool:
         res = pool.map(
             dis_counts,
-            valid_files
+            valid_files,
+            repeat(filter_str, len(valid_files))
         )
     # --- collect data ---
     ares = list(res)
